b0d5f37c5375ba77846ee9f62911aaa2229a0065
hritik7388 <choreohritik52@gmail.com>
2026-07-10 13:19:30 +0530

    chore: clean up .gitignore by removing unnecessary entries and consolidating logs

--- dist/shared/enums/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserType = void 0;
-var UserType;
-(function (UserType) {
-    UserType["USER"] = "USER";
-    UserType["SUPER_ADMIN"] = "SUPERADMIN";
-    UserType["SUB_ADMIN"] = "SUBADMIN";
-})(UserType || (exports.UserType = UserType = {}));
