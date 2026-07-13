0788d9956411f5f2b81dcd391359968534786041
hritik7388 <choreohritik52@gmail.com>
2026-07-13 11:29:53 +0530

    push the code for the  otp generator

--- src/common/utils/otp.ts
+++ src/common/utils/otp.ts
@@ -1,9 +1,17 @@
 // src/common/utils/otp.ts
+import crypto from "node:crypto";
+
+export const OTP_EXPIRY_SECONDS = 3 * 60; // 3 minutes
+
 export const generateOTP = (length: number = 6): string => {
   const digits = "0123456789";
   let otp = "";
+
+  const randomBytes = crypto.randomBytes(length);
+
   for (let i = 0; i < length; i++) {
-    otp += digits[Math.floor(Math.random() * 10)];
+    otp += digits[randomBytes[i] % digits.length];
   }
+
   return otp;
-}
+};
